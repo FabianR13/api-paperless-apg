3c2cc17069f4651ed99be14b3cc096bd278ab56f
FabianR13 <fabian.ramos@apgmexico.mx>
2022-09-30 09:51:02 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 // import pkg from '../package.json' assert {type: 'json'};
-import express from "express";
-import morgan from "morgan";
+// import express from "express";
+// import morgan from "morgan";
+const express =requiere("express");
+const morgan = require("morgan");
 // import pkg from "../package.json";
-import config from "./config.js";
+// import config from "./config.js";
 // import pkg from "../package.json" assert { type: "json" };
 // import("../package.json", { assert: { type: "json" } });
 
