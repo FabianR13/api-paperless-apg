3e0535d1dd9fe13b54cf68746465611a7c0c4918
FabianR13 <fabian.ramos@apgmexico.mx>
2022-10-04 13:41:15 -0500

    Update uploadProfileImg.js

--- src/middlewares/uploadProfileImg.js
+++ src/middlewares/uploadProfileImg.js
@@ -1,22 +1,36 @@
-const multer = require("multer");
-const shortid = require("shortid");
-// import multer from "multer";
-// import shortid from "shortid";
+   const AWS = require('aws-sdk');
+   const multer = require('multer');
+   const multerS3 = require('multer-s3');
+   const shortid = require("shortid");
+   const dotenv = require('dotenv')
+   dotenv.config({path:"C:\\api-paperless-apg\\src\\.env"});
+   
+   AWS.config.update({
+     region: process.env.S3_BUCKET_REGION,
+     apiVersion: 'latest',
+     credentials: {
+     accessKeyId: process.env.S3_ACCESS_KEY,
+     secretAccessKey: process.env.S3_SECRET_ACCESS_KEY
+     }
+   })
+   
+   const s3 = new AWS.S3();
+   
+   const upload = multer({
+     storage: multerS3({
+         s3,
+         bucket:process.env.S3_BUCKET_NAME + "/Uploads/Employees",
+         metadata: (req, file, cb) => {
+             cb(null, { fieldName: file.fieldname });
+         },
+         key: (req, file, cb) => {
+             cb(null, shortid.generate() + ".jpeg");
+         }
+     })
+   });
 
-// Save Locally the Files
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "C:\\Paperless\\PAPERLESS-APG\\public\\Uploads\\Employees");
-      // cb(null, "E:\\Paperless\\PAPERLESS-APG\\build\\Uploads\\Employees");
-    },
-    filename: function (req, file, cb) {
-      cb(null, shortid.generate() + ".jpeg");
-    },
-  });
-  
-  const upload = multer({ storage }); //Single Field Form
-  
-   // Upload Multiple Fields Form
+
+
   module.export = upload.single('picture');
   
   
