0ab3570ddaff62ce504ac73170c868733b831bca
MahonriRincon <dmahonri@hotmail.com>
2023-08-09 15:28:07 -0600

    Metodo Inicial de Envio de correo
    Metodo en archivo principal para enviar un correo electronico, version inicial

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
-    SECRET:'paperless-apg-api-chida'
-    //MAIL_AUTH_USER:'',
-    //MAIL_AUTH_PASS:''
+    SECRET:'paperless-apg-api-chida',
+    MAIL_AUTH_USER:'paperless@apgmexico.mx',
+    MAIL_AUTH_PASS:'sjvljdwwytdxbsfv'
 }
 
 module.exports = config;
