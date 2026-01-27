bc34c61ef52784cb23dc1760ee2f0183c72591bc
FabianR13 <fabian.ramos@apgmexico.mx>
2026-01-27 16:42:29 -0600

    uipdate supermarket y kaizen

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ require("./database"); // Al requerirlo, se ejecuta tu conexión a Mongo automá
 
 // 1. IMPORTAMOS LA FUNCIÓN DEL BOT
 // (Asegúrate que la ruta sea correcta. Si index.js está en 'src', esto está bien)
-const { connectDiscordBot } = require("./discord/bot.js"); 
+// const { connectDiscordBot } = require("./discord/bot.js"); 
 
 app.set('port', process.env.PORT || 4000);
 
@@ -14,5 +14,5 @@ app.listen(app.get('port'), () => {
     
     // 2. INICIAMOS EL BOT DE DISCORD
     // Lo ponemos aquí para que arranque junto con el servidor
-    connectDiscordBot();
+    // connectDiscordBot();
 });
