1f79b362ec80610968a13c6f026936dba35a3bcf
FabianR13 <fabian.ramos@apgmexico.mx>
2026-07-07 12:21:31 -0600

    update

--- src/routes/quarantine.routes.js
+++ src/routes/quarantine.routes.js
@@ -1,29 +1,33 @@
 const { Router } = require("express");
 const { createQUARequest, getAllRegisters, updateRegisters, releaseFromQuarantine } = require("../controllers/quarantine.controller");
-const { verifyToken, isAutorized} = require("../middlewares/auth.Jwt");
+const { verifyToken, isAutorized, isQuarantineC, isQuarantineR} = require("../middlewares/auth.Jwt");
 
 const router = Router();
 router.post("/NewRecord/:CompanyId",
     verifyToken,
     isAutorized,
+    isQuarantineC,
     createQUARequest
 );
 
 router.get("/GetRecords/:CompanyId",
     verifyToken,
     isAutorized,
+    isQuarantineR,
     getAllRegisters
 );
 
 router.put("/updateRegisters/:CompanyId/:moveID",
     verifyToken,
     isAutorized,
+    isQuarantineC,
     updateRegisters
 );
 
 router.put("/release/:CompanyId",
     verifyToken,
     isAutorized,
+    isQuarantineC,
     releaseFromQuarantine
 );
 
