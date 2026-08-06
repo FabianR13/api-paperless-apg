7fc90c5283822cc35a20649923784febb58af8aa
FabianR13 <fabian.ramos@apgmexico.mx>
2026-08-06 12:11:12 -0600

    Actualizacion con control de paneles

--- src/routes/panels.routes.js
+++ src/routes/panels.routes.js
@@ -1,6 +1,8 @@
 const { Router } = require('express');
 const router = Router();
-const { abrirPanelRemoto, sincronizarLogsPanel } = require('../controllers/panels.controller.js');
+const { abrirPanelRemoto, sincronizarLogsPanel, configurarHorarioPanel, enviarCredencialUnica,
+    sincronizarTodoElPanel,eliminarCredencialUnica
+} = require('../controllers/panels.controller.js');
 
 // Ruta: POST /api/paneles/abrir
 router.post('/abrir', abrirPanelRemoto);
@@ -11,5 +13,10 @@ const panelCtrl = require('../controllers/panels.controller');
 
 router.get('/', panelCtrl.getPaneles);
 router.post('/', panelCtrl.crearPanel);
+
+router.post('/configurar-horario', configurarHorarioPanel);
+router.post('/credencial', enviarCredencialUnica);
+router.post('/sync-masiva', sincronizarTodoElPanel)
+router.post('/eliminar-credencial', eliminarCredencialUnica);
 
 module.exports = router;
